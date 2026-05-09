67ef8752ddd59c35acb5902b3d751bebdcb60947
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-05-09 08:54:09 +0530

    added files

---
+++ Javascript/basics/Variables.js
@@ -0,0 +1,26 @@
+// Var, Let & Const 
+
+var a =10;
+var a =20;
+console.log(a); // 20 is output
+
+// let b =10;
+// let b =20; 
+// console.log(b)
+// SyntaxError: Identifier 'b' has already been declared
+
+
+// We cant declear the let but can reassign the value in it 
+let c =10;
+c =20;
+console.log(c)
+// 20 is output 
+
+
+// We can only declear at once and only assign value once
+const d =20;
+d =20
+console.log(d) //TypeError: Assignment to constant variable.
+
+
+
