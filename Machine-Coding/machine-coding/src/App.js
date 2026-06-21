6d82629ca1046344f10957ef874d5e72d8d07a32
Ssharma0355 <ssharma0355@gmail.com>
2026-06-21 14:02:43 +0530

    d

--- Machine-Coding/machine-coding/src/App.js
+++ Machine-Coding/machine-coding/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import FormTab from './Problems/Tab-Form/FormTab'
-import ValidForm from './Problems/Form-Validation/ValidForm'
-import Pagination from './Problems/Pagination/Pagination'
-import Board from './Problems/tic-tac-toe/Board'
+import React from "react";
+import FormTab from "./Problems/Tab-Form/FormTab";
+import ValidForm from "./Problems/Form-Validation/ValidForm";
+import Pagination from "./Problems/Pagination/Pagination";
+import Board from "./Problems/tic-tac-toe/Board";
+import QueryTodo from "./React-Query/QueryTodo";
+// import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 function App() {
+  // const queryClient = new QueryClient();
+
   return (
     <div>
-      <Board />
+  
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
