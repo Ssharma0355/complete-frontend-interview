078f7dbfd6c52d8a7933444610ae954ebee4cc2d
Ssharma0355 <ssharma0355@gmail.com>
2026-06-22 13:21:48 +0530

    map

---
+++ Javascript/DSA/HashMap/HashMap.js
@@ -0,0 +1,12 @@
+const m = new Map()
+// Hash Map created like this
+
+
+// Adding value
+m.set(10,300);
+m.set(4,"sachin")
+m.set("sharma",1000)
+m.set("DSA", true)
+
+console.log(m.get("DSA"))
+// access the map 
