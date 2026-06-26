98416bfe3a82a90158632fd6e6139589766ca02c
Ssharma0355 <ssharma0355@gmail.com>
2026-06-26 15:07:15 +0530

    reuable Progress bar

--- Machine-Coding/machine-coding/src/App.js
+++ Machine-Coding/machine-coding/src/App.js
@@ -6,6 +6,7 @@ import Board from "./Problems/tic-tac-toe/Board";
 import Prod from "./Revision/Prod";
 import FileExp from "./Problems/File-Explorer/FileExp";
 import Timer from "./Problems/Timer/Timer";
+import ParentProgress from "./Problems/ProgressBar/ParentProgress";
 // import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 function App() {
@@ -14,8 +15,9 @@ function App() {
   return (
     <div>
       {/* <Prod /> */}
-      <FileExp />
+      {/* <FileExp /> */}
       {/* <Timer /> */}
+      <ParentProgress />
   
     </div>
   );
---
+++ Machine-Coding/machine-coding/src/Problems/ProgressBar/ParentProgress.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Progressbar from './Progressbar'
+
+function ParentProgress() {
+  return (
+    <div>
+      ParentProgress
+      <Progressbar value={88} />
+    </div>
+  )
+}
+
+export default ParentProgress
