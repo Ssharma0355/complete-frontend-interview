7ca4f9c8e64bd2d9ec58fb6f4bacecd5d44d0788
Ssharma0355 <ssharma0355@gmail.com>
2026-06-26 13:26:01 +0530

    file

--- Machine-Coding/machine-coding/src/App.js
+++ Machine-Coding/machine-coding/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <div>
       {/* <Prod /> */}
-      {/* <FileExp /> */}
-      <Timer />
+      <FileExp />
+      {/* <Timer /> */}
   
     </div>
   );
--- Machine-Coding/machine-coding/src/Problems/File-Explorer/FileExp.jsx
+++ Machine-Coding/machine-coding/src/Problems/File-Explorer/FileExp.jsx
@@ -1,39 +1,62 @@
-import React, { useState } from 'react'
-import json from "./data.json"
+import React, { useState } from "react";
+import json from "./data.json";
 
 function FileExp() {
-    const [data, setData] = useState(json);
-    console.log(data);
+  const [data, setData] = useState(json);
+  console.log(data);
 
+  const List = ({ list }) => {
+    const [isExpland, setIsExpland] = useState({});
 
-
-    const List =({list})=>{
-        const [isExpland, setIsExpland] = useState(false)
-        const openIt =()=>{
-            setIsExpland(prev => !prev)
-        }
-        return(
-            <div >
-                {list.map((node)=>
-                <div key={node.id}>
-                    {node?.isFolder && <button onClick={openIt}> {isExpland?"+":"-"} </button>}
-                    <span>{node.name}</span>
-                    {isExpland && node?.children && 
-                    <div style={{marginLeft:"20px", borderTop:"1px dotted gray", padding:"12px"}}>
-                    <List list={node.children} />
-                    <List list={node.children} />
-
-                    </div>}
-                 </div>
-                    )}
-            </div>
-        )
-    }
+    return (
+      <div>
+        {list.map((node) => (
+          <div key={node.id}>
+            {node?.isFolder && (
+              <button
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setIsExpland((prev) => ({
+                    ...prev,
+                    [node?.name]: !prev[node?.name],
+                  }));
+                }}
+              >
+                {" "}
+                {isExpland?.[node.name] ? "+" : "-"}{" "}
+              </button>
+            )}
+            <span>
+              {node.name}{" "}
+              {node.isFolder ? <button style={{marginLeft:"12px"}}>Add Folder</button> : ""}
+            </span>
+            {isExpland?.[node.name] && node?.children && (
+              <div
+                style={{
+                  marginLeft: "20px",
+                  borderTop: "1px dotted gray",
+                  padding: "12px",
+                }}
+              >
+                <List list={node.children} />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
   return (
-    <div style={{display:"flex", flexDirection:"column", border:"1px solid black"}}>
-        <List list={data}/>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        border: "1px solid black",
+      }}
+    >
+      <List list={data} />
     </div>
-  )
+  );
 }
 
-export default FileExp
+export default FileExp;
