4f1e3567b21ac6b30f85529242792f78593855a7
Ssharma0355 <ssharma0355@gmail.com>
2026-06-27 23:19:44 +0530

    ds

--- Machine-Coding/machine-coding/src/Problems/ProgressBar/ParentProgress.jsx
+++ Machine-Coding/machine-coding/src/Problems/ProgressBar/ParentProgress.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Progressbar from './Progressbar'
 
 function ParentProgress() {
+    const [progress, setProgress] = useState(0)
+    const [isProgress, setIsProgress] = useState(true)
+    useEffect(()=>{
+        if(progress >=100){
+            setProgress(0)
+        }
+       
+        const Timer = setInterval(()=>{
+            
+            setProgress(prev=> prev+10)
+        },1000)
+
+        return()=>{clearInterval(Timer)}
+
+    },[isProgress,progress])
+
+    // useEffect(()=>{
+    //     if(progress >=100){
+    //         setProgress(0)
+    //     }
+
+    // },[progress])
+    
   return (
     <div>
       ParentProgress
-      <Progressbar value={88} />
+      <Progressbar value={progress} />
     </div>
   )
 }
